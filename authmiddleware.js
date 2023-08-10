46cefae792dad2cae84b5b0d5e6ee1fec6acdc32
cmsjy040317 <cmsjy040317@naver.com>
2023-08-10 10:14:12 +0900

    2023-08-10

---
+++ authmiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = require('./secret_key.js');
+
+const authMiddleware = async function(request, response, next) {
+	console.log(request.url);
+	const accessToken = request.headers['token'];
+	if (accessToken == null) {
+		response.status(403).json({success:false, errormessage:'토큰이 존재하지않습니다.'});
+	} else {
+		try {
+			const tokenInfo = await jwt.verify(accessToken, SECRET_KEY);
+			request.tokenInfo = tokenInfo;
+			next();
+		} catch(err) {
+			response.status(403).json({success:false, errormessage:'토큰인증 실패'});
+		}
+	}
+}
+
+module.exports = authMiddleware;
